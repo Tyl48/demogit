51f3dee8700c07741f346ea3d9cb0d7e3bcee388
Tyl48 <Nguyentruongthinh.9876@gmail.com>
2023-09-19 09:46:02 +0700

    sjdhf

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.ForegroundColor= ConsoleColor.Yellow;
+            Console.BackgroundColor= ConsoleColor.Green;
+            Console.WriteLine("chuong trinh tim kiem ");
+            Console.WriteLine("tim kiem theo ten ");
+            Console.WriteLine("tim kiem theo tac gia ");
+            Console.WriteLine("tim kiem theo nha xuat ban ");
+            Console.WriteLine("tim kiem theo tieu de ");
+
+            Console.ReadKey();
+            */
+
+
+
+            int n;
+            Console.WriteLine("nhap vao 1 so ");
+            n = int.Parse(Console.ReadLine());
+
+            switch (n)
+            {
+                case 1: Console.WriteLine(" ct tim kiem ten ");
+                    break;
+                case 2: Console.WriteLine(" ct tim kiem tac gia ");
+                    break;
+                case 3: Console.WriteLine("ct tim kiem nxb ");
+                    break;
+                default: Console.WriteLine(" khong hop le ");
+                    break;
+           }
+
+            Console.ReadKey();  
+
+        }
+    }
+}
